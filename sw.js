14efa9ed696e3e1e22d49f2cecb8121ccf92a21c
TradePilot24 <flexi025@gmail.com>
2026-07-01 22:38:36 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE="tradepilot-genesis-v1";const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./assets/logo.png"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
